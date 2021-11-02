5e2df76825a1e6f26da71a67e76fc238ffb560eb
= <=>
2021-11-01 22:06:48 -0500

    try

--- process.py
+++ process.py
@@ -155,9 +155,10 @@ def process(log_file, sub_process, fecha_inicial, fecha_final, fecha_inicial_pr,
                         with open(filename) as f_osbj:
                             for line in f_obj:
                                 parameters = np.append(parameters, str(line.rstrip()))
-                        validacion = sepp_model.validation_model(parameters[0].astype(np.float), parameters[1].astype(np.float))
-                        print(parameters[0], parameters[1])
-                        print(validacion)
+                        parameters=np.asfarray(parameters,float)
+                        #validacion = sepp_model.validation_model(parameters[0], parameters[1].)
+                        print(type(parameters[0]), type(parameters[1]))
+                        #print(validacion)
                     #if f_final_pr =
                 
     except Exception as e:
