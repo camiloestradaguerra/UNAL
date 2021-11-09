a91897519cfd0e73b91bcb07c64eb1616187102f
= <=>
2021-11-09 18:45:06 -0500

    summary.text

--- examples.py
+++ examples.py
@@ -6,8 +6,6 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import shutup 
 shutup.please()
 
-"--summary_file '/home/unal/percepcion/Resultados/examples/example_download.log' "
-
 def example1():
     print("Ejemplo para el procesamiento de los datos: Extracción de los datos de la API, limpieza y preparacion de los datos para el proceso entrenamiento")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-05 23:59:59' ")
@@ -18,7 +16,7 @@ def example2():
 
 def example3():
     print("Ejemplo para el proceso de prediccion")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'predict' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 13:59:59' " + "--fecha_inicial_pr '2021-01-04 00:00:00' " + "--fecha_final_pr '2021-01-04 09:59:00' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--subprocess 'predict' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 13:59:59' " + "--fecha_inicial_pr '2021-01-04 00:00:00' " + "--fecha_final_pr '2021-01-04 09:59:00' ")
 
 def example4():
     print("Ejemplo para el proceso de validacion")
