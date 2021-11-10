da45fdbfabefb3a708ae89ffda337bebb50f62ed
= <=>
2021-11-09 19:18:39 -0500

    summary.txt clean

--- examples.py
+++ examples.py
@@ -6,20 +6,20 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import shutup 
 shutup.please()
 
-def example1():
+def example2():
     print("Ejemplo para el procesamiento de los datos: Extracción de los datos de la API, limpieza y preparacion de los datos para el proceso entrenamiento")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-05 23:59:59' ")
 
-def example2():
+def example3():
     print("Ejemplo para el proceso de entrenamiento")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'train' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 13:59:59' ")
 
-def example3():
+def example4():
     print("Ejemplo para el proceso de prediccion")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'predict' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 13:59:59' " + "--fecha_inicial_pr '2021-01-04 00:00:00' " + "--fecha_final_pr '2021-01-04 09:59:00' ")
 
-def example4():
+def example5():
     print("Ejemplo para el proceso de validacion")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'validation' " + "--fecha_inicial '2021-01-04 00:00:00' " + "--fecha_final '2021-01-04 09:59:00' ")
 
-example3()
+example2()
