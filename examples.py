a6ba538f9ad5d3c3fdefafca8f51e0bf68356383
= <=>
2021-11-16 11:34:48 -0500

    clean

--- examples.py
+++ examples.py
@@ -13,7 +13,7 @@ def example1():
 
 def example2():
     print("Ejemplo para el procesamiento de los datos: Extracción de los datos de la API, limpieza y preparacion de los datos para el proceso entrenamiento")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-05 23:59:59' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-02-01 00:00:00' " + "--fecha_final '2021-09-30 23:59:59' ")
 
 def example3():
     print("Ejemplo para el proceso de entrenamiento")
@@ -27,4 +27,4 @@ def example5():
     print("Ejemplo para el proceso de validacion")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'validation' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 13:59:59' "+ "--fecha_inicial_pr '2021-01-04 00:00:00' " + "--fecha_final_pr '2021-01-04 09:59:00' ")
 
-example4()
+example2()
