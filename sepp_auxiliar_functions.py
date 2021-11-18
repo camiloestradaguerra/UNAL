a6cd54936f14dcccd9833b91506223d6111b11ee
= <=>
2021-11-18 17:57:15 -0500

    pred

--- sepp_auxiliar_functions.py
+++ sepp_auxiliar_functions.py
@@ -516,7 +516,7 @@ def filtering_data(percentage_area, array_cells_events_tst_data_1_cells, two_dim
 
     #puntos_gdf_cells_4326_2['Fecha'] = puntos_gdf_cells_4326_2['Fecha'].map(without_milisecond)
     #print(type(puntos_gdf_cells_4326.Fecha.iloc[0]))
-
+    print(puntos_gdf_cells_4326)
     puntos_gdf_cells_4326.to_file("predicted_events.geojson", driver='GeoJSON')
     
     number_ev_pred_on_hotspots = np.array([])
