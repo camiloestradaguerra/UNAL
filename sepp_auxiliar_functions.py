0e536d6d0640f5658ca72a95a9355a13ea8f98d9
= <=>
2021-11-18 12:34:06 -0500

    pred

--- sepp_auxiliar_functions.py
+++ sepp_auxiliar_functions.py
@@ -491,9 +491,9 @@ def filtering_data(percentage_area, array_cells_events_tst_data_1_cells, two_dim
         return txt[:19]
 
     print(type(puntos_gdf_cells_4326.Fecha.iloc[0]))
-    puntos_gdf_cells_4326_2 = puntos_gdf_cells_4326
-    puntos_gdf_cells_4326_2['Fecha'] = puntos_gdf_cells_4326_2['Fecha'].map(FECHA_mod)
-    print(type(puntos_gdf_cells_4326_2.Fecha.iloc[0]))
+    #puntos_gdf_cells_4326_2 = puntos_gdf_cells_4326
+    #puntos_gdf_cells_4326_2['Fecha'] = puntos_gdf_cells_4326_2['Fecha'].map(FECHA_mod)
+    #print(type(puntos_gdf_cells_4326_2.Fecha.iloc[0]))
 
     #puntos_gdf_cells_4326_2['Fecha'] = puntos_gdf_cells_4326_2['Fecha'].map(without_milisecond)
     
