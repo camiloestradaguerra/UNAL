856e9b374c4d541a3b7c4b968ef10eb6919c920e
= <=>
2021-11-21 11:55:36 -0500

    train

--- examples.py
+++ examples.py
@@ -9,11 +9,11 @@ shutup.please()
 
 def example1():
     print("Ejemplo para la creación de los poligonos de la red sobre el mapa de Bogotá.")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'poligonos_covariados' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-05 23:59:59' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'poligonos_covariados' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-31 23:59:59' ")
 
 def example2():
     print("Ejemplo para el procesamiento de los datos: Extracción de los datos de la API, limpieza y preparacion de los datos para el proceso entrenamiento")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-03 23:59:59' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-07 23:59:59' ")
 
 def example3():
     print("Ejemplo para el proceso de entrenamiento")
