5a9d5bb7148bd1a5e6eda17d3fc66d05f40c99f0
= <=>
2021-11-21 20:43:08 -0500

    clean

--- examples.py
+++ examples.py
@@ -13,7 +13,7 @@ def example1():
 
 def example2():
     print("Ejemplo para el procesamiento de los datos: Extracción de los datos de la API, limpieza y preparacion de los datos para el proceso entrenamiento")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-07 23:59:59' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'clean' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-02-07 23:59:59' ")
 
 def example3():
     print("Ejemplo para el proceso de entrenamiento")
@@ -25,6 +25,6 @@ def example4():
 
 def example5():
     print("Ejemplo para el proceso de validacion")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'validation' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-20 23:59:59' "+ "--fecha_inicial_pr '2021-01-21 00:00:00' " + "--fecha_final_pr '2021-01-26 23:59:59' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'validation' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-01-31 23:59:59' "+ "--fecha_inicial_pr '2021-01-21 00:00:00' " + "--fecha_final_pr '2021-01-26 23:59:59' ")
 
-example5()
+example2()
