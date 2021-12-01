386f251c95ba9599fd7a3dd41484888f0ad32e04
= <=>
2021-12-01 11:28:53 -0500

    pred

--- examples.py
+++ examples.py
@@ -21,10 +21,10 @@ def example3():
 
 def example4():
     print("Ejemplo para el proceso de prediccion")
-    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'predict' " + "--fecha_inicial '2021-02-22 00:00:00' " + "--fecha_final '2021-02-28 23:59:59' " + "--fecha_inicial_pr '2021-01-04 00:00:00' " + "--fecha_final_pr '2021-01-04 09:59:00' ")
+    os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'predict' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-02-21 23:59:59' " + "--fecha_inicial_pr '2021-02-22 00:00:00' " + "--fecha_final_pr '2021-02-28 23:59:59' ")
 
 def example5():
     print("Ejemplo para el proceso de validacion")
     os.system("python process.py " + "--log_file 'example_log.log' " + "--summary_file 'example_summary.log' " + "--subprocess 'validation' " + "--fecha_inicial '2021-01-01 00:00:00' " + "--fecha_final '2021-02-21 23:59:59' "+ "--fecha_inicial_pr '2021-02-21 00:00:00' " + "--fecha_final_pr '2021-02-28 23:59:59' ")
 
-example3()
+example4()
