e705ada4d22ebf9a415891099aa7adb7a1cfbd4d
= <=>
2021-12-20 12:11:05 -0500

    train

--- process.py
+++ process.py
@@ -74,9 +74,11 @@ def process(log_file, summary_file, sub_process, fecha_inicial, fecha_final, fec
                 def FECHA_mod(txt):
                     return txt.replace("T"," ")
                 file = open("fechas_entrenamiento.txt", "w")
-                file.write(FECHA_mod(str(datos_eventos.FECHA.iloc[0])) + '\n')
-                file.write(FECHA_mod(str(datos_eventos.FECHA.iloc[-1])) + '\n')
+                file.write(str(fecha_inicial) + '\n')
+                file.write(str(fecha_final) + '\n')
                 file.close()
+                #file.write(FECHA_mod(str(datos_eventos.FECHA.iloc[0])) + '\n')
+                #file.write(FECHA_mod(str(datos_eventos.FECHA.iloc[-1])) + '\n')
                 # Entrena el modelo con los datos y crea un archivo con los parametros optimizados
                 parametros_opt = sepp_model.train_model(datos_eventos)
                 summary = open(summary_file,"a+")
