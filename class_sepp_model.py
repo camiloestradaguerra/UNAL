47e421e442db62b62ce5f198386cbb93066e1d48
= <=>
2021-12-19 20:53:50 -0500

    pred

--- class_sepp_model.py
+++ class_sepp_model.py
@@ -405,9 +405,9 @@ class ModeloRinhas(ModeloBase):
                 for k in range(0, len(cov_norm_cell_m)):
                     random.seed(7)
                     ev = simulate(window_size, k)[0]
-                    print(ev, k)
                     # events of all cells
                     all_events_sim = np.append(all_events_sim, ev)
+                    print(all_events_sim)
                     # number of event per cell
                     events_sim_on_cells = np.array(np.append(events_sim_on_cells, len(ev)),int)
                 
